210d2dd140450a167173f4692c126c3f14fc922e
EstherAuma <estheraumaego@gmail.com>
2026-09-19 13:21:46 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'products-new',
+  webDir: 'www'
+};
+
+export default config;
