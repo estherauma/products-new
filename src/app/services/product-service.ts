be3b6205d7db035a5cc51a06a81d85f18b0acd50
EstherAuma <estheraumaego@gmail.com>
2026-09-19 13:55:12 +0300

    add a function for creating product in the service

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -1,4 +1,33 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import {Storage} from '@ionic/storage-angular'
 
-@Service()
-export class ProductService {}
+export interface Product {
+    id:number,
+    name:string,
+    category:number,
+    unitPrice:number,
+    quantity:number
+}
+
+const PRODUCT_KEY = 'products'
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+    private storageReady: Promise<Storage>;
+    constructor(private storage:Storage) {
+        this.storageReady = this.storage.create();
+    }
+
+    ready() {
+        return this.storageReady;
+    }
+
+    async createProduct(product:Product) {
+        await this.ready();
+        const products:Product[] = await this.storage.get(PRODUCT_KEY) || [];
+        product.id = products.length > 0 ? Math.max(...products.map(p => p.id)) + 1 : 1;
+        products.push(product);
+        await this.storage.set(PRODUCT_KEY,products);
+    }
+}
